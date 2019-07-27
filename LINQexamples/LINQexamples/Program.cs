cd3604d30180f6df07859b54dfe8e2558477e0d1
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-27 10:19:10 +0600

    Product class & Display static function

--- LINQexamples/LINQexamples/Program.cs
+++ LINQexamples/LINQexamples/Program.cs
@@ -7,9 +7,27 @@ using System.Threading.Tasks;
 namespace LINQexamples
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            var products = new List<Product>()
+            {
+                new Product(){Id=1,Name="Chair",Price=500},
+                new Product(){Id=2,Name="Table",Price=1500},
+                new Product(){Id=3,Name="Bed",Price=2500},
+                new Product(){Id=4,Name="Door",Price=5000},
+                new Product(){Id=5,Name="Window",Price=3500}
+            };
+            Display(products);
+            Console.ReadKey();
         }
+        static private void Display(dynamic products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine("Id: " + product.Id + " Name: " + product.Name + " Price: " + product.Price + Environment.NewLine);
+            }
+        }
+
     }
 }
