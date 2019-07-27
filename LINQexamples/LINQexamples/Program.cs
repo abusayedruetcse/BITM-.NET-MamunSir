0bf8a7a9d6ee1b873cc4dc3f89ffd6682d70931f
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-27 14:17:18 +0600

    Linq: LINQ101 Projection Select is added

--- LINQexamples/LINQexamples/Program.cs
+++ LINQexamples/LINQexamples/Program.cs
@@ -14,14 +14,15 @@ namespace LINQexamples
             {
                 new Product(){Id=1,Name="Chair",Price=500},
                 new Product(){Id=2,Name="Table",Price=1500},
-                new Product(){Id=3,Name="Bed",Price=2500},
-                new Product(){Id=4,Name="Door",Price=5000},
+                new Product(){Id=3,Name="Door",Price=2500},
+                new Product(){Id=4,Name="Bed",Price=5000},
                 new Product(){Id=5,Name="Window",Price=3500},
                 new Product(){Id=6,Name="Bed",Price=2000}
             };
             //Display(products);
             //SelectAll(products);
-            UsingWhereMethod(products);
+            //UsingWhereMethod(products);
+            LINQ101ProjectionSelect(products);
             Console.ReadKey();
         }
         static private void Display(dynamic products)
@@ -36,13 +37,13 @@ namespace LINQexamples
             //1.select p
             Console.WriteLine("select p\n\n");
             var selectedProductList = from p in products
-                                      where p.Price > 1000 & p.Price < 3000
+                                      where p.Price > 1000 && p.Price < 3000
                                       select p;                                     
             Display(selectedProductList);
             //2.select p.Name
             Console.WriteLine("select p.Name\n\n");
             var selectedProductNameList = from p in products
-                                      where p.Price > 1000 & p.Price < 3000
+                                      where p.Price > 1000 && p.Price < 3000
                                       select p.Name;
             foreach (var p in selectedProductNameList)
             {
@@ -51,7 +52,7 @@ namespace LINQexamples
             //3.select anonymous obj
             Console.WriteLine("select anonymous obj\n\n");
             var selectedProductAnonymousList = from p in products
-                                               where p.Price > 1000 & p.Price < 3000
+                                               where p.Price > 1000 && p.Price < 3000
                                                select new { Name = p.Name, Price = Convert.ToInt32(p.Price) - 50 };
             foreach(var p in selectedProductAnonymousList)
             {
@@ -60,7 +61,7 @@ namespace LINQexamples
             //4.orderby p.Name
             Console.WriteLine("orderby p.Name\n\n");
             var orderbyProductList = from p in products
-                                     where p.Price > 0 & p.Price < 5001
+                                     where p.Price > 0 && p.Price < 5001
                                      orderby p.Name
                                      //orderby p.Name,p.Price
                                      select p;
@@ -68,7 +69,7 @@ namespace LINQexamples
             //5.group by p.Name
             Console.WriteLine("group by p.Name\n\n");
             var groupbyNameProductList = from p in products
-                                         where p.Price > 0 & p.Price < 5001
+                                         where p.Price > 0 && p.Price < 5001
                                          group p by p.Name into g
                                          orderby g.Key
                                          select g;
@@ -83,7 +84,7 @@ namespace LINQexamples
             //  Another way
             Console.WriteLine("Another way\n\n");
             var groupbyNameProductAnonymousList = from p in products
-                                                  where p.Price > 0 & p.Price < 5001
+                                                  where p.Price > 0 && p.Price < 5001
                                                   group p by p.Name into g
                                                   select new { key = g.Key, Products = g.ToList() };
             foreach (var g in groupbyNameProductAnonymousList)
@@ -106,19 +107,19 @@ namespace LINQexamples
             //1. select p
             Console.WriteLine("1. select p\n\n");
             var selectedProductList = products
-                                            .Where(p => p.Price > 1000 & p.Price < 3000);                                           
+                                            .Where(p => p.Price > 1000 && p.Price < 3000);                                           
             Display(selectedProductList);
             //   Another way 
             Console.WriteLine("1. Another way \n\n");
             var selectedProductAnotherList = products
-                                            .Where(p => p.Price > 1000 & p.Price < 3000)
+                                            .Where(p => p.Price > 1000 && p.Price < 3000)
                                             .Select(p => p);
             Display(selectedProductAnotherList);
 
             //2.select p.Name
             Console.WriteLine("2.select p.Name\n\n");
             var selectedProductNameList = products
-                                                .Where(p => p.Price > 1000 & p.Price < 3000)
+                                                .Where(p => p.Price > 1000 && p.Price < 3000)
                                                 .Select(p => p.Name);
             foreach (var p in selectedProductNameList)
             {
@@ -127,7 +128,7 @@ namespace LINQexamples
             //3.select anonymous obj
             Console.WriteLine("select anonymous obj\n\n");
             var selectedProductAnonymousList = products
-                                                    .Where(p => p.Price > 1000 & p.Price < 3000)
+                                                    .Where(p => p.Price > 1000 && p.Price < 3000)
                                                     .Select(p=> new { Name = p.Name, Price = Convert.ToInt32(p.Price) - 50 });                                              
             foreach (var p in selectedProductAnonymousList)
             {
@@ -136,14 +137,14 @@ namespace LINQexamples
             //4.orderby p.Name
             Console.WriteLine("4.orderby p.Name\n\n");
             var orderbyProductList = products
-                                          .Where(p => p.Price > 0 & p.Price < 5001)
+                                          .Where(p => p.Price > 0 && p.Price < 5001)
                                           .OrderByDescending(p => p.Name)
                                           .Select(p=>p);                                 
             Display(orderbyProductList);
             //5.group by p.Name
             Console.WriteLine("5.group by p.Name\n\n");
             var groupbyNameProductList = products
-                                              .Where(p => p.Price > 0 & p.Price < 5001)
+                                              .Where(p => p.Price > 0 && p.Price < 5001)
                                               .GroupBy(p => p.Name)
                                               .OrderBy(p => p.Key)
                                               //.Select(p=>p)
@@ -156,6 +157,42 @@ namespace LINQexamples
                     Console.WriteLine("\t\tId: " + pg.Id + " Name: " + pg.Name + " Price: " + pg.Price + Environment.NewLine);
                 }
             }
+        }     
+        static private void LINQ101ProjectionSelect(List<Product> products)
+        {
+            //1. nested situation for two collections
+            Console.WriteLine("1. nested situation for two collections\n\n");
+            int[] positions = new int[] { 1, 3, 5 };
+            string[] numbers = new string[] { "zero","one", "two", "three", "four", "five", "six" };
+            var textNum = from ii in positions
+                          select numbers[ii];
+            foreach(var num in textNum)
+            {
+                Console.WriteLine(num + Environment.NewLine);
+            }
+            //2. Operation on product
+            Console.WriteLine("2. Operation on product\n\n");
+            var operationOnProductList = from p in products
+                                         select new { Upper = p.Name.ToUpper(), Lower = p.Name.ToLower() };
+            foreach(var p in operationOnProductList)
+            {
+                Console.WriteLine("Upper: {0}, Lower: {1}\n", p.Upper, p.Lower);
+            }
+            //3. Using Index of Collection
+            Console.WriteLine("3. Using Index of Collection\n\n");
+            var indexedList = products
+                                .Select((p,index)=>new {Product=p.Name, Inplace=p.Name.Length==index });  //index for index of list element
+            foreach(var p in indexedList)
+            {
+                Console.WriteLine(p.Product+" Place: "+p.Inplace+Environment.NewLine);
+            }
+            //4. Compound two from clause
+            Console.WriteLine("4. Compound two from clause\n\n");
+            var TwoTableCombineList = from index in positions
+                                      from p in products
+                                      where p.Id == index
+                                      select p;
+            Display(TwoTableCombineList);
         }
 
 
